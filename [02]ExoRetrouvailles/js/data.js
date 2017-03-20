fd32e59189eed75284cba9da81b82ed0f75e1912
charlzyy <charles.bergheaud@lepoles.com>
2017-03-20 17:08:00 +0100

    ajout javascript

---
+++ [02]ExoRetrouvailles/js/data.js
@@ -0,0 +1,12 @@
+var data = {
+  'perso_0' : {
+    'name' : 'luffy',
+    'picture' : 'img/1.jpg',
+    'texte' : ['Regarde quand meme et admire!!!', ' Hey zorro j\ai appris une nouvelle technique tu veux voir ?','GOMU GOMU NO !!! ...']
+    },
+  'perso_1' : {
+    'name' : 'zorro',
+    'picture' : 'img/zoro.png',
+    'texte' : ['Capitaine!','Vite alors !','Non je vais aller boire du sake!']
+  }
+}
